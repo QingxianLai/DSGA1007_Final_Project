84026dd632b91a19e45ecf1e7dcf348a95142233
jianming Zhou <diymyfuture@gmail.com>
2014-12-11 17:44:18 -0500

    zhou

---
+++ NYC_Official_Jobs/pieplot.py
@@ -0,0 +1,25 @@
+from jobDescription import degreeDict
+import matplotlib.pyplot as plt
+from pylab import *
+
+
+
+
+def plot_Degree(dataframe):
+    """plot the percent of job that needed a degree"""
+    degree=degreeDict(dataframe)
+    bach=degree['Bachelor']
+    master=degree['Master']
+    others=degree['High School or others non-degree']
+    
+    # make a square figure and axes
+    figure(2, figsize=(6,6))
+    ax = axes([0.1, 0.1, 0.8,0.8])
+    # The slices will be ordered and plotted counter-clockwise.
+    labels = 'At least \n a Bachelor degree', 'At least\n   A Master \n degree','Others:\nHigh school \nDiploma or \nSome experiences '
+    fracs = [bach, master, others]
+    explode=(0.05, 0.05, 0.05)
+    pie(fracs, explode=explode, labels=labels,autopct='%1.1f%%', shadow=True, startangle=90)
+    title('Minimum Degree Requirement', bbox={'facecolor':'1.0', 'pad':5})
+    show()
+
