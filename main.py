63d995f71769aed7de41aaddcc26ce3a36e307c3
QingxianLai <ql516@nyu.edu>
2014-12-11 17:23:56 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+__author__ = 'LaiQX'
+
+import numpy as np
+import pandas as pd
+
+
+def main():
+
+
+    Job_data = safely_input()
+    Job_data = clean_data()
+    Overall_analysis(Job_data)
+    Job_list = filter_data()
+    Interested_jobs_anaysis(Job_list)
+    show_certain_job(Job_list)
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
