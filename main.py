81f106b3928cdf2d3fe3434f20d406ed55c60262
QingxianLai <ql516@nyu.edu>
2014-12-12 17:44:30 -0500

    overall analysis

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     print ">>>",
     Job_data = Clean_df(raw_data)
     print "data cleaned >>>"
-    overall_analysis(Job_data)
+    job_list = overall_analysis(Job_data)
 #    Job_list = filter_data(Job_data)
 #    Interested_jobs_anaysis(Job_list)
 #    show_certain_job(Job_list)
